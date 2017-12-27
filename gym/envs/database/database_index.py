446572cfe73bded63b4dff1893f5ae5cf1304bff
Ronny Li <ronnyli91@gmail.com>
2017-12-27 18:51:06 -0500

    WIP

--- gym/envs/database/database_index.py
+++ gym/envs/database/database_index.py
@@ -1,5 +1,6 @@
 """
 Set indices for a database table to better handle the queries performed against it
+TODO: modify to work on a single query at a time (for transfer learning)
 """
 
 import logging
@@ -24,16 +25,18 @@ class DatabaseIndexEnv(gym.Env):
         self.port = port
         self.closed = True
         self.conn = self.connect_db()
-        # Target that we are optimizing for
-        self.schema = schema
-        self.table = table
-        self.column_stats = None
         # Optimize table on these queries
+        # DataFrame w/ following columns: query, schema, table, frequency, priority
+        # frequency and priority might end up as features in the state
         self.queries = queries
+        self.current_index = 0
+        # contains various statistics stored by the database on its tables
+        self.column_stats = None
+        self.table_rows = None
+        self.get_stats()
 
         # Execution times to calculate reward
-        self.initial_execution_time = None
-        self.previous_execution_time = None
+        self.previous_cost = None
 
         self.action_space = self.determine_action_space()
         self.observation_space = self.determine_observation_space()
@@ -59,32 +62,46 @@ class DatabaseIndexEnv(gym.Env):
 
     def determine_action_space(self):
         '''
-        Action space consists of every column in table
-        with each column having a boolean for whether to include it in the index
-        and a rank for sorting the order of the column (if included).
+        Action space consists of up to 32 columns with a binary flag to indicate
+        if the column should be included in the index or not and a continuous
+        rank to indicate what order the columns should be in the index.
+
+        If a table has more than 32 columns, select the ones with highest cardinality
+        for the action space.
 
         TODO: need to figure out how to handle when multiple columns are sampled
         to have the same rank
         '''
-        if self.closed: self.connect_db()
+        self.action_space = spaces.Tuple((
+            spaces.MultiBinary(32),
+            spaces.Box(low=0, high=1, shape=(32,))
+        ))
 
+    def get_stats(self):
+        tables = self.queries['table'].unique()
+        schemas = self.queries['schema'].unique()
         self.column_stats = pd.read_sql('''
-            SELECT *
+            SELECT pg_stats.schemaname
+                 , pg_stats.tablename
+                 , pg_stats.attname AS colname
+                 , CASE
+                   WHEN pg_stats.n_distinct >= 0 THEN pg_stats.n_distinct
+                   -- otherwise n_distinct is the negative proportion of cardinality
+                   ELSE -1 * pg_stats.n_distinct * pg_stat_user_tables.n_live_tup
+                    END AS n_distinct
+                 , pg_stat_user_tables.n_live_tup AS n_row
               FROM pg_stats
-             WHERE schemaname = '{schema}'
-               AND tablename = '{table}'
-        '''.format(schema=self.schema, table=self.table), self.conn)
+              JOIN pg_stat_user_tables
+                ON pg_stats.schemaname = pg_stat_user_tables.schemaname
+               AND pg_stats.tablename = pg_stat_user_tables.relname
+             WHERE pg_stats.schemaname IN ({schemas})
+               AND pg_stats.tablename IN ({tables})
+        '''.format(schemas=','.join("'{0}}'".format(s) for s in schemas),
+                   tables=','.join("'{0}'".format(t) for t in table)),
+            self.conn)
+        self.table_rows = self.column_stats[['schemaname', 'tablename', 'n_row']].drop_duplicates()
 
-        assert self.column_stats.attname.is_unique, 'columns are not unique'
-
-        self.action_space = spaces.Dict({
-            colname: spaces.Dict({
-                'is_included': spaces.Discrete(2),
-                'rank': spaces.Box(low=0, high=1, shape=(1,))
-            }) for colname in self.column_stats.attname
-        })
-
-    def create_index(self, action):
+    def create_index(self, action, schema, table):
         '''
         Convert action to SQL
 
@@ -92,16 +109,18 @@ class DatabaseIndexEnv(gym.Env):
         used in the index and a rank variable that is used for
         determining their order in the index
         '''
-        included_cols = []
-        for col, col_action in sorted(action.iteritems(),
-                                      key=lambda (c,a): a['rank'],
-                                      reverse=True):
-            if col_action['is_included'] == 1:
-                included_cols.append('"{}"'.format(col))
-        stmt = 'CREATE INDEX ON {schema}.{table} ({cols})'.format(cols=', '.join(included_cols),
-                                                                  schema=self.schema,
-                                                                  table=self.table)
-        self.conn.cursor().execute(stmt)
+        self.column_cost['is_included'] = action[0]
+        self.column_cost['rank'] = action[1]
+
+        included_cols = self.column_cost \
+            .sort('rank', ascending=False) \
+            .loc[self.column_cost['is_included'] == 1, 'colname']
+
+        if len(included_cols) > 0:
+            stmt = 'CREATE INDEX ON {schema}.{table} ({cols})'.format(cols=', '.join(included_cols),
+                                                                      schema=schema,
+                                                                      table=table)
+            self.conn.cursor().execute(stmt)
 
     def query_indexes(self):
         '''
@@ -123,8 +142,10 @@ class DatabaseIndexEnv(gym.Env):
         - for each column, what is its cardinality? (unchanging wrt to query and action)
         - for each column, what was the cost associated with it in the query?
         - how many steps were involved in the query?
+
+        # TODO: modify to work on a single query (Probably a Tuple this time)
         '''
-        ncol = self.column_stats.attname.shape[0]
+        ncol = self.column_stats.colname.shape[0]
         nquery = len(self.queries)
         obs_shape = (nquery, 1 + (ncol * 2))
         return spaces.Box(
@@ -134,9 +155,9 @@ class DatabaseIndexEnv(gym.Env):
 
     def get_cardinality(self):
         '''
-        Returns a pandas dataframe with columns attname (aka colname) and n_distinct
+        Returns a pandas dataframe with columns colname (aka colname) and n_distinct
         '''
-        colstat = self.column_stats[['attname', 'n_distinct']]
+        colstat = self.column_stats[['colname', 'n_distinct']]
         if colstat.n_distinct.min() < 0:
             # n_distinct is negative if Postgres believes the cardinality is proportional to row_count
             # Replace negative n_distinct values with approximation of actual cardinality
@@ -147,22 +168,24 @@ class DatabaseIndexEnv(gym.Env):
             assert colstat.n_distinct.min() >= 0
         return colstat
 
-    def get_column_cost(self, query):
+    def get_column_cost(self, schema, table, query):
         '''
         Return column presence in the query plan. If found, attribute the
         Total Cost - Startup Cost to this column.
         '''
         plan = self.explain(query)
         flat = self.preprocess_query_plan(plan)
-        columns = pd.DataFrame(self.column_stats.attname)
+        columns = self.column_stats.loc[self.column_stats['schemaname'] == schema &
+                                            self.column_stats['tablename'] == table,
+                                        ['colname', 'n_distinct']]
         columns['cost'] = 0
-        for col in columns.attname:
+        for col in columns.colname:
             isin_filter = flat['Filter'].str.contains(col).fillna(False)
             isin_index = flat['Index Cond'].str.contains(col).fillna(False)
             indicator = isin_filter | isin_index
             if indicator.any():
-                columns.loc[columns['attname'] == col, 'cost'] = flat[indicator]['Node Cost'].sum()
-        return columns
+                columns.loc[columns['colname'] == col, 'cost'] = flat[indicator]['Node Cost'].sum()
+        self.column_cost = columns.sort('n_distinct', ascending=False)
 
     def preprocess_query_plan(self, plan):
         '''
@@ -206,58 +229,72 @@ class DatabaseIndexEnv(gym.Env):
         assert 'frequency' in queries.columns
         self.queries = queries
 
-    def set_initial_state(self):
-        assert self.queries is not None, 'must set_queries'
-        cardinality = self.get_cardinality()
-        for query in self.queries['query']:
-            column_cost = self.get_column_cost(query)
-            query_cost = self.explain(query)['Plan']['Total Cost']
-
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def _step(self, action):
+        print 'step start'
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
         state = self.state
-        x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action==1 else -self.force_mag
-        costheta = math.cos(theta)
-        sintheta = math.sin(theta)
-        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        x  = x + self.tau * x_dot
-        x_dot = x_dot + self.tau * xacc
-        theta = theta + self.tau * theta_dot
-        theta_dot = theta_dot + self.tau * thetaacc
-        self.state = (x,x_dot,theta,theta_dot)
-        done =  x < -self.x_threshold \
-                or x > self.x_threshold \
-                or theta < -self.theta_threshold_radians \
-                or theta > self.theta_threshold_radians
-        done = bool(done)
 
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warning("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-            self.steps_beyond_done += 1
-            reward = 0.0
+        current_query_record = self.queries.loc[self.current_index]
+        query, schema, table, frequency = self.parse_query_record(current_query_record)
+
+        previous_cost = self.previous_cost  # cost before action
+        self.create_index(action, schema, table)  # action
+        cost_after = self.get_query_cost(query)  # cost after action
+
+        complexity_penalty = 0  # unnecessary if you run test over insert queries as well
+        reward = previous_cost - cost_after - complexity_penalty
+
+        self.current_index += 1
+        next_query_record = self.queries.loc[self.current_index]
+        self.set_state(next_query_record)
+
+        done = self.current_index == self.queries.shape[0]
 
         return np.array(self.state), reward, done, {}
 
     def _reset(self):
+        print 'reset'
         # Undo all changes made by model
         self.conn.rollback()
-        self.state = self.initial_state
+        self.random_sort_queries()
+        self.current_index = 0
+        first_query_record = self.queries.loc[self.current_index]
+        self.set_state(first_query_record)
         self.steps_beyond_done = None
         return np.array(self.state)
 
+    def set_state(self, query_record):
+        '''
+        The features that are observed, which are affected by actions
+        '''
+        query, schema, table, frequency = self.parse_query_record(query_record)
+
+        self.get_column_cost(schema, table, query)
+        self.previous_cost = self.get_query_cost(query)
+
+        self.state = column_cost \
+            [['n_distinct', 'cost']] \
+            .head(32)
+
+    def parse_query_record(self, query_record):
+        query = query_record['query']
+        schema = query_record['schema']
+        table = query_record['table']
+        frequency = query_record['frequency']
+        return (query, schema, table, frequency)
+
     def _render(self, mode='human', close=False):
         pass
+
+    def get_query_cost(self, query):
+        return self.explain(query)['Plan']['Total Cost']
+
+    def random_sort_queries(self):
+        '''
+        Re-order queries so the model does not fit on it
+        '''
+        self.queries = self.queries.sample(frac=1).reset_index(drop=True)
